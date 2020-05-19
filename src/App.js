4a2339496b15be92352de967d8f35190acbdefac
alexrpedersen <alex.r.pedersen@outlook.com>
2020-05-19 15:13:19 -0700

    adds basic folders

--- src/App.js
+++ src/App.js
@@ -1,49 +1,19 @@
 import React from 'react';
-import Button from './button.js';
-import Footer from './footer';
-import Header from './header';
-import './App.css';
+import Button from './components/button/button.js';
+// import './App.scss';
+import Header from './components/header/header.js';
+import Footer from './components/footer/footer.js';
+
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-  }
-
-  incrementCount = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
-  };
-
-  decrementCount = () => {
-    this.setState({
-      count: this.state.count - 1
-    });
-  };
-
   render() {
-    let { count } = this.state;
     return (
-    <React.Fragment>
-      <div className="app">
-        <Header />
-        <div>
-          <div class="count">
-            <h3>Count:</h3>
-            <h1>{count}</h1>
-          </div>
-          <div class="buttons">
-            <Button title={"-"} action={this.decrementCount} />
-            <Button title={"+"} action={this.incrementCount} />
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </React.Fragment>
-    );
+      <>
+       <Header />
+       <Button />
+       <Footer />
+      </>
+    )
   }
 }
 
---
+++ src/components/button/button.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+ class Button extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrementCount = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  render() {
+    return (
+      <div className="Button">
+        <div>
+          <div class="count">
+            <h3>Count:</h3>
+            <h1>{this.state.count}</h1>
+          </div>
+          <button onClick={this.decrementCount}> - </button>
+          <button onClick={this.incrementCount}> + </button>
+        </div>
+      </div>
+      
+   
+    );
+  };
+ };
+
+export default Button;
