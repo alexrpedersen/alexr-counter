d5622b64060d1e113043002a73db9d9f2ecd4b0a
alexrpedersen <alex.r.pedersen@outlook.com>
2020-05-18 23:09:36 -0700

    its all working!!!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from "./button.js";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrementCount = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  render() {
+    let { count } = this.state;
+    return (
+    <React.Fragment>
+      <div className="app">
+        <div>
+          <div class="count">
+            <h3>Count:</h3>
+            <h1>{count}</h1>
+          </div>
+          <div class="buttons">
+            <Button title={"-"} action={this.decrementCount} />
+            <Button title={"+"} action={this.incrementCount} />
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+    );
+  }
 }
 
 export default App;
